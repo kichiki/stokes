81ebd64082b2e85198360e4407e38d20dde96907
kengo ichiki <kengoichiki@gmail.com>
2006-10-23 17:18:11 +0000

    bug fixed on bool values in scheme...

--- src/xi3.scm
+++ src/xi3.scm
@@ -1,15 +1,15 @@
 ; sample set file for xi3
 ; SC lattice config of 8 particles in (5,5,5) box
-; $Id: xi3.scm,v 1.1 2006/10/21 19:05:27 kichiki Exp $
+; $Id: xi3.scm,v 1.2 2006/10/23 17:18:11 kichiki Exp $
 (define version    "F")     ; version. "F", "FT", or "FTS"
-(define flag-mat   t)       ; t => matrix scheme, nil => atimes scheme
-(define flag-notbl nil)     ; t => no-table,      nil => with table
+(define flag-mat   #t)      ; #t => matrix scheme, #f => atimes scheme
+(define flag-notbl #f)      ; #t => no-table,      #f => with table
 
 (define np         8)       ; number of particles
 (define ewald-eps  1.0e-12) ; cut-off limit for Ewald summation
 
 ; lattice vector
-(define lattice '(5.0 5.0 5.0))
+(define lattice '(5.0  5.0  5.0))
 
 ; configuration of particles
 (define x #(
