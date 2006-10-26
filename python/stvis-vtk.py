dbf9c827e04bee28ecdeb2174e2579539618fd3a
kengo ichiki <kengoichiki@gmail.com>
2006-10-26 02:42:10 +0000

    some minor style change.

--- python/stvis-vtk.py
+++ python/stvis-vtk.py
@@ -1,6 +1,6 @@
-# test code for libstokes
+# visualization program for stokes-nc file by VTK
 # Copyright (C) 2006 Kengo Ichiki <kichiki@users.sourceforge.net>
-# $Id: stvis-vtk.py,v 1.1 2006/10/26 02:19:15 kichiki Exp $
+# $Id: stvis-vtk.py,v 1.2 2006/10/26 02:42:10 kichiki Exp $
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -38,106 +38,111 @@ def pData_read(np, x):
 
 
 def usage():
-    print '$Id: stvis-vtk.py,v 1.1 2006/10/26 02:19:15 kichiki Exp $'
+    print '$Id: stvis-vtk.py,v 1.2 2006/10/26 02:42:10 kichiki Exp $'
     print 'USAGE:'
     print '\t-f or --file : stokes-nc-file'
     sys.exit ()
 
-filename = ''
-i = 1
-while i < len(sys.argv):
-    if sys.argv[i] == '-f' or sys.argv[i] == '--file':
-        filename = sys.argv[i+1]
-        i += 2
-    else:
-        usage()
-if filename == '': usage()
 
-#nc = stokes.stokes_nc_open ("test.nc")
-nc = stokes.stokes_nc_open (filename)
-#stokes.stokes_nc_print_actives(nc, stokes.get_stdout())
-print 'ntime = ', nc.ntime
-lattice = stokes.darray(3)
-stokes.stokes_nc_get_l (nc, lattice)
-print 'lattice = ', lattice[0], lattice[1], lattice[2]
-if lattice[0] > lattice[2]:
-    l = lattice[0]
-else:                  
-    l = lattice[2]
+def main():
+    filename = ''
+    i = 1
+    while i < len(sys.argv):
+        if sys.argv[i] == '-f' or sys.argv[i] == '--file':
+            filename = sys.argv[i+1]
+            i += 2
+        else:
+            usage()
+    if filename == '': usage()
 
-x  = stokes.darray(nc.np  * nc.nvec)
+    nc = stokes.stokes_nc_open (filename)
+    #stokes.stokes_nc_print_actives(nc, stokes.get_stdout())
+    print 'ntime = ', nc.ntime
+    lattice = stokes.darray(3)
+    stokes.stokes_nc_get_l (nc, lattice)
+    print 'lattice = ', lattice[0], lattice[1], lattice[2]
+    if lattice[0] > lattice[2]:
+        l = lattice[0]
+    else:                  
+        l = lattice[2]
+
+    x  = stokes.darray(nc.np  * nc.nvec)
 
 
-# then, make Actor
-pSource = vtk.vtkSphereSource()
-#pSource.SetPhiResolution(20)
-#pSource.SetThetaResolution(20)
+    # then, make Actor
+    pSource = vtk.vtkSphereSource()
+    # pSource.SetPhiResolution(20)
+    # pSource.SetThetaResolution(20)
 
-pGlyph = vtk.vtkGlyph3D()
-pGlyph.SetSource(pSource.GetOutput())
-#pGlyph.SetInput(pData)
-pGlyph.ScalingOn()
-pGlyph.SetScaleModeToScaleByScalar()
+    pGlyph = vtk.vtkGlyph3D()
+    pGlyph.SetSource(pSource.GetOutput())
+    # pGlyph.SetInput(pData)
+    pGlyph.ScalingOn()
+    pGlyph.SetScaleModeToScaleByScalar()
 
-pMapper = vtk.vtkPolyDataMapper()
-pMapper.ScalarVisibilityOff()
-pMapper.SetInput(pGlyph.GetOutput())
+    pMapper = vtk.vtkPolyDataMapper()
+    pMapper.ScalarVisibilityOff()
+    pMapper.SetInput(pGlyph.GetOutput())
 
-pActor = vtk.vtkActor()
-pActor.SetMapper(pMapper)
+    pActor = vtk.vtkActor()
+    pActor.SetMapper(pMapper)
 
-pActor.GetProperty().SetColor(1,1,1)
-pActor.GetProperty().SetOpacity(1)
+    pActor.GetProperty().SetColor(1,1,1)
+    pActor.GetProperty().SetOpacity(1)
 
-# fixed particles
-if nc.npf > 0:
-    print 'npf = ', nc.npf
-    xf0  = stokes.darray(nc.npf * nc.nvec)
-    stokes.stokes_nc_get_data0 (nc, "xf0", xf0)
-    pfData = pData_read(nc.npf, xf0)
+    # fixed particles
+    if nc.npf > 0:
+        print 'npf = ', nc.npf
+        xf0  = stokes.darray(nc.npf * nc.nvec)
+        stokes.stokes_nc_get_data0 (nc, "xf0", xf0)
+        pfData = pData_read(nc.npf, xf0)
 
-    pfGlyph = vtk.vtkGlyph3D()
-    pfGlyph.SetSource(pSource.GetOutput()) # use the same pSource
-    pfGlyph.SetInput(pfData)
-    pfGlyph.ScalingOn()
-    pfGlyph.SetScaleModeToScaleByScalar()
+        pfGlyph = vtk.vtkGlyph3D()
+        pfGlyph.SetSource(pSource.GetOutput()) # use the same pSource
+        pfGlyph.SetInput(pfData)
+        pfGlyph.ScalingOn()
+        pfGlyph.SetScaleModeToScaleByScalar()
 
-    pfMapper = vtk.vtkPolyDataMapper()
-    pfMapper.ScalarVisibilityOff()
-    pfMapper.SetInput(pfGlyph.GetOutput())
+        pfMapper = vtk.vtkPolyDataMapper()
+        pfMapper.ScalarVisibilityOff()
+        pfMapper.SetInput(pfGlyph.GetOutput())
 
-    pfActor = vtk.vtkActor()
-    pfActor.SetMapper(pfMapper)
-    # put Red color
-    pfActor.GetProperty().SetColor(1,0,0)
-    pfActor.GetProperty().SetOpacity(1)
+        pfActor = vtk.vtkActor()
+        pfActor.SetMapper(pfMapper)
+        # put Red color
+        pfActor.GetProperty().SetColor(1,0,0)
+        pfActor.GetProperty().SetOpacity(1)
 
     
-## prepare renderer
-ren = vtk.vtkRenderer()
-win = vtk.vtkRenderWindow()
-win.AddRenderer(ren)
-#iren = vtk.vtkRenderWindowInteractor ()
-#iren.SetRenderWindow(win)
+    ## prepare renderer
+    ren = vtk.vtkRenderer()
+    win = vtk.vtkRenderWindow()
+    win.AddRenderer(ren)
+    # iren = vtk.vtkRenderWindowInteractor ()
+    # iren.SetRenderWindow(win)
 
-ren.AddActor(pActor)
-if nc.npf > 0: ren.AddActor(pfActor)
-ren.SetBackground (0.1, 0.2, 0.4)
-win.SetSize(480,360)
+    ren.AddActor(pActor)
+    if nc.npf > 0: ren.AddActor(pfActor)
+    ren.SetBackground (0.1, 0.2, 0.4)
+    win.SetSize(480,360)
 
-aCamera = vtk.vtkCamera()
-ren.SetActiveCamera (aCamera)
-aCamera.SetFocalPoint (0.5*lattice[0], 0, 0.2*lattice[2])
-aCamera.SetPosition (0.5*lattice[0], 2.0*l, 0.1*lattice[2])
+    aCamera = vtk.vtkCamera()
+    ren.SetActiveCamera (aCamera)
+    aCamera.SetFocalPoint (0.5*lattice[0], 0, 0.2*lattice[2])
+    aCamera.SetPosition (0.5*lattice[0], 2.0*l, 0.1*lattice[2])
 
-# loop
-while 1:
-    for i in range(nc.ntime):
-        stokes.stokes_nc_get_data (nc, "x", i, x)
+    # loop
+    while 1:
+        for i in range(nc.ntime):
+            stokes.stokes_nc_get_data (nc, "x", i, x)
 
-        pData = pData_read(nc.np, x)
-        pGlyph.SetInput(pData)
-        win.Render()
+            pData = pData_read(nc.np, x)
+            pGlyph.SetInput(pData)
+            win.Render()
+
+
+if __name__ == "__main__":
+    main()
 
 
   
