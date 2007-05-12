5b994fab48e11630f26b3f8e42c8aac9fd801e66
kengo ichiki <kengoichiki@gmail.com>
2007-05-12 04:35:21 +0000

    add imposed flow informations.

--- python/stncdump.py
+++ python/stncdump.py
@@ -1,6 +1,6 @@
 # dump stokes-netcdf
 # Copyright (C) 2007 Kengo Ichiki <kichiki@users.sourceforge.net>
-# $Id: stncdump.py,v 1.2 2007/04/20 02:23:12 kichiki Exp $
+# $Id: stncdump.py,v 1.3 2007/05/12 04:35:21 kichiki Exp $
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -21,7 +21,7 @@ import stokes
 
 
 def usage():
-    print '$Id: stncdump.py,v 1.2 2007/04/20 02:23:12 kichiki Exp $'
+    print '$Id: stncdump.py,v 1.3 2007/05/12 04:35:21 kichiki Exp $'
     print 'USAGE:'
     print '\t-f or --file : stokes-nc-file'
     print '\t-line        : all particles are in a single line for each time'\
@@ -48,13 +48,26 @@ def main():
     stokes.stokes_nc_print_actives(nc, stokes.get_stdout())
     print ''
     
+    # imposed flows
+    if nc.flag_ui0 == 1:
+        ui0 = stokes.darray(3)
+        stokes.stokes_nc_get_array1d (nc, "Ui0", ui0)
+        print 'ui0 = %f %f %f'%(ui0[0], ui0[1], ui0[2])
+    if nc.flag_oi0 == 1:
+        oi0 = stokes.darray(3)
+        stokes.stokes_nc_get_array1d (nc, "Oi0", oi0)
+        print 'oi0 = %f %f %f'%(oi0[0], oi0[1], oi0[2])
+    if nc.flag_ei0 == 1:
+        ei0 = stokes.darray(5)
+        stokes.stokes_nc_get_array1d (nc, "Ei0", ei0)
+        print 'ei0 = %f %f %f %f %f'%(ei0[0], ei0[1], ei0[2], ei0[3], ei0[4])
     lattice = stokes.darray(3)
     stokes.stokes_nc_get_l (nc, lattice)
     print 'lattice %f %f %f\n'%(lattice[0], lattice[3], lattice[2])
 
     pos = stokes.darray(nc.np  * nc.nvec)
 
-    # print the infor about the output format
+    # print the infos about the output format
     if flag_line == 0:
         print '# t, i, x, y, z'
     else:
