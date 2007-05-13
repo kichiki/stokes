a9a6556fd43230d72b1e7ef7a11eefb8758abc99
kengo ichiki <kengoichiki@gmail.com>
2007-05-13 23:14:10 +0000

    new particle style 'T_CHECKER' designed for the mobile particles with quaternion.

--- python/stnc2pov.py
+++ python/stnc2pov.py
@@ -1,6 +1,6 @@
 # stokes-netcdf to pov converter
 # Copyright (C) 2006-2007 Kengo Ichiki <kichiki@users.sourceforge.net>
-# $Id: stnc2pov.py,v 1.4 2007/05/12 04:37:47 kichiki Exp $
+# $Id: stnc2pov.py,v 1.5 2007/05/13 23:14:10 kichiki Exp $
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -18,6 +18,57 @@
 import sys
 #sys.path.append('/somewhere/ryuon/stokes/python')
 import stokes
+
+
+def write_T_Particle(f):
+    f.write('#declare T_Particle = texture {\n'\
+            '  pigment { color White }\n'\
+            '  //finish { ambient 0.2 diffuse 0 reflection 0.6 }\n'\
+            '  finish {\n'\
+            '    ambient .1\n'\
+            '    diffuse .1\n'\
+            '    specular 1\n'\
+            '    roughness .001\n'\
+            '    metallic\n'\
+            '    reflection {\n'\
+            '      .75\n'\
+            '      metallic\n'\
+            '    }\n'\
+            '  }\n'\
+            '}\n')
+
+def write_M_RYUON(f):
+    # M_RYUON
+    f.write('#declare M_RYUON = material {\n'\
+            '  texture {\n'\
+            '    pigment {\n'\
+            '      color <0.4, 0.5, 1.0>\n'\
+            '      filter 1\n'\
+            '    }\n'\
+            '    finish {\n'\
+            '      ambient 0\n'\
+            '      diffuse 0\n'\
+            '      reflection .25\n'\
+            '      specular 1\n'\
+            '      roughness .001\n'\
+            '    }\n'\
+            '  } // end of texture\n'\
+            '  interior { ior 1.33 }\n'
+            '}\n')
+
+def write_T_CHECKER(f):
+    f.write('#declare T_CHECKER = texture {\n'\
+            '  pigment{\n'\
+            '    checker\n'\
+            '    color <0.4, 0.5, 1.0>\n'\
+            '    color White\n'\
+            '  }\n'\
+            '  scale 0.01\n'\
+            '  finish{\n'\
+            '    phong 0.9\n'\
+            '    metallic\n'\
+            '  }\n'\
+            '}\n')
 
 
 def bounding_box (np, x):
@@ -82,32 +133,59 @@ def write_pov_header (f, lattice, camera, lookat):
     laz = lookat[1]/100.0
     lay = lookat[2]/100.0
 
-    f.write('#include \"colors.inc\"\n')
-    f.write('#include "woods.inc"\n\n')
+    f.write('#include "colors.inc"\n')
+    #f.write('#include "woods.inc"\n\n')
     # place the ground
-    f.write('// floor\nplane {\n  y, 0\n  texture { T_Wood6 }\n}\n')
+    f.write('// floor\nplane {\n'\
+            '  y, -0.1\n'\
+            '  texture {\n'\
+            #'    T_Wood6\n'\
+            #'    finish{ ambient 1 }\n'\
+            '    pigment { checker color White, color <.7, .7, .7> }\n'\
+            '    scale .3\n'\
+            '    finish{ ambient .4 }\n'\
+            '  }\n'\
+            '}\n')
     # place the walls
-    f.write('// back wall\nplane {\n  z, 1\n  pigment { color rgb <1,1,0.8> }\n}\n')
-    f.write('// ceiling\nplane {\n  y, 5\n  pigment { color White }\n}\n')
-    f.write('// right wall\nplane {\n  x, 5\n  pigment { color White }\n}\n')
-    f.write('// left wall\nplane {\n  x, -5\n  pigment { color White }\n}\n')
-    f.write('// behind wall\nplane {\n  z, -5\n  pigment { color White }\n}\n\n')
+    f.write('// back wall\n'\
+            'plane {\n'\
+            '  z, 1\n'\
+            '  pigment { color rgb <1,1,0.8> }\n'\
+            '  finish{ ambient 0.4 }\n'\
+            '}\n')
+    f.write('// ceiling\n'\
+            'plane {\n'\
+            '  y, 5\n'\
+            '  pigment { color White }\n'\
+            '}\n')
+    f.write('// right wall\n'\
+            'plane {\n'\
+            '  x, 5\n'\
+            '  pigment { color White }\n'\
+            '}\n')
+    f.write('// left wall\n'\
+            'plane {\n'\
+            '  x, -5\n'\
+            '  pigment { color White }\n'\
+            '}\n')
+    f.write('// behind wall\n'\
+            'plane {\n  z, -5\n'\
+            '  pigment { color White }\n'\
+            '}\n\n')
     # place the box
-    f.write('box {\n  <0, 0, 0>,  // Near lower left corner\n  <%f, %f, %f>   // Far upper right corner\n  pigment { color rgbf <0.9, 0.99, 1, 1> }\n}\n\n'%(lx, ly, lz))
-    #
+    f.write('box {\n'\
+            '  <0, 0, 0>,  // Near lower left corner\n'\
+            '  <%f, %f, %f>   // Far upper right corner\n'\
+            '  pigment { color rgbf <0.9, 0.99, 1, 1> }\n'\
+            '}\n\n'%(lx, ly, lz))
+
     f.write('camera {\n  location <%f, %f, %f>\n'%(cx, cy, cz))
     f.write('  look_at  <%f, %f,  %f>\n}\n\n'%(lax, lay, laz))
     f.write('light_source { <2, 4.9, -3> color White}\n\n')
-    f.write('#declare T_Particle = texture {\n  pigment { color White }\n  //finish { ambient 0.2 diffuse 0 reflection 0.6 }\n  finish {\n    ambient .1\n    diffuse .1\n    specular 1\n    roughness .001\n    metallic\n    reflection {\n      .75\n      metallic\n    }\n  }\n}')
 
-def write_pov_particle (f, x, y, z, a):
-    # note that in POVRAY,
-    # y is the vertical direction
-    # z is the depth direction
-    # scale factor = 1/100 (0.01 radius = 1 in POV)
-    f.write('sphere {\n')
-    f.write('  <%f, %f, %f>, %f\n'%(x/100.0, z/100.0, y/100.0, a/100.0))
-    f.write('  texture { T_Particle }\n}\n');
+    write_T_Particle(f)
+    write_M_RYUON (f)
+    write_T_CHECKER(f)
 
 
 # INPUT
@@ -135,15 +213,25 @@ def write_pov_header_open (f, lattice, camera, lookat):
     f.write('#include \"colors.inc\"\n')
     f.write('#include "woods.inc"\n\n')
     # place the walls
-    f.write('// back wall\nplane {\n  z, 2\n  pigment { checker color White, color <0.6, 0.8, 1> }\n  scale 0.1\n}\n')
-    f.write('// behind wall\nplane {\n  z, -5\n  pigment { color White }\n}\n\n')
+    f.write('// back wall\n'\
+            'plane {\n'\
+            '  z, 2\n'\
+            '  pigment { checker color White, color <0.6, 0.8, 1> }\n'\
+            '  scale 0.1\n}\n')
+    f.write('// behind wall\n'\
+            'plane {\n'\
+            '  z, -5\n'\
+            '  pigment { color White }\n'\
+            '}\n\n')
     #
     f.write('camera {\n  location <%f, %f, %f>\n'%(cx, cy, cz))
     f.write('  look_at  <%f, %f,  %f>\n}\n\n'%(lax, lay, laz))
     f.write('light_source { <2, 4.9, -3> color White}\n\n')
-    f.write('#declare T_Particle = texture {\n  pigment { color White }\n  //finish { ambient 0.2 diffuse 0 reflection 0.6 }\n  finish {\n    ambient .1\n    diffuse .1\n    specular 1\n    roughness .001\n    metallic\n    reflection {\n      .75\n      metallic\n    }\n  }\n}')
-    # M_RYUON
-    f.write('#declare M_RYUON = material {\n  texture {\n    pigment {\n      color <0.4, 0.5, 1.0>\n      filter 1\n    }\n    finish {\n      ambient 0\n      diffuse 0\n      reflection .25\n      specular 1\n      roughness .001\n    }\n  } // end of texture\n  interior { ior 1.33 }\n}\n')
+
+    write_T_Particle(f)
+    write_M_RYUON (f)
+    write_T_CHECKER(f)
+
 
 def write_pov_particle (f, x, y, z, a):
     # note that in POVRAY,
@@ -152,7 +240,6 @@ def write_pov_particle (f, x, y, z, a):
     # scale factor = 1/100 (0.01 radius = 1 in POV)
     f.write('sphere {\n')
     f.write('  <%f, %f, %f>, %f\n'%(x/100.0, z/100.0, y/100.0, a/100.0))
-    #f.write('  texture { T_Particle }\n}\n')
     f.write('  material { M_RYUON }\n}\n')
 
 def write_pov_particle_fixed (f, x, y, z, a):
@@ -163,8 +250,61 @@ def write_pov_particle_fixed (f, x, y, z, a):
     f.write('sphere {\n')
     f.write('  <%f, %f, %f>, %f\n'%(x/100.0, z/100.0, y/100.0, a/100.0))
     f.write('  texture { T_Particle }\n}\n')
-    #f.write('  material { M_RYUON }\n}\n')
 
+
+# make transform matrix (3x3) by quaternion
+def Q2M (q1,q2,q3,q4):
+    m = []
+
+    # parity change
+    q4 *= -1.0
+
+    m.append(2.0*(q1*q1 + q4*q4 - .5))
+    m.append(2.0*(q1*q2 + q3*q4))
+    m.append(2.0*(q1*q3 - q2*q4))
+
+    m.append(2.0*(q1*q2 - q3*q4))
+    m.append(2.0*(q2*q2 + q4*q4 - .5))
+    m.append(2.0*(q2*q3 + q1*q4))
+
+    m.append(2.0*(q1*q3 + q2*q4))
+    m.append(2.0*(q2*q3 - q1*q4))
+    m.append(2.0*(q3*q3 + q4*q4 - .5))
+
+    # note that in POVRAY,
+    # y is the vertical direction
+    # z is the depth direction
+    t = [1.0, 0.0, 0.0,\
+         0.0, 0.0, 1.0,\
+         0.0, 1.0, 0.0]
+    x = [0.0, 0.0, 0.0,  0.0, 0.0, 0.0,  0.0, 0.0, 0.0]
+    y = [0.0, 0.0, 0.0,  0.0, 0.0, 0.0,  0.0, 0.0, 0.0]
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                x[i*3+j] += t[i*3+k] * m[k*3+j]
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                y[i*3+j] += x[i*3+k] * t[k*3+j]
+    # therefore, Y = T . M . T
+    return y
+
+def write_pov_particle_Q (f, x, y, z, a, q):
+    # note that in POVRAY,
+    # y is the vertical direction
+    # z is the depth direction
+    # scale factor = 1/100 (0.01 radius = 1 in POV)
+    m = Q2M (q[0], q[1], q[2], q[3])
+    f.write('sphere {\n')
+    f.write('  <0, 0, 0>, %f\n'%(a/100.0))
+    f.write('  texture { T_CHECKER }\n')
+    f.write('  transform {\n')
+    f.write('    matrix <%f, %f, %f,\n'%(m[0], m[3], m[6]))
+    f.write('            %f, %f, %f,\n'%(m[1], m[4], m[7]))
+    f.write('            %f, %f, %f,\n'%(m[2], m[5], m[8]))
+    f.write('            %f, %f, %f> }\n}\n'%(x/100.0, z/100.0, y/100.0))
+    
 
 # now camera angle
 # init:   <0.17,  0.50, -1.10>  <0.17,  0.50, 0.0>
@@ -185,10 +325,9 @@ def move_camera (camera, lookat):
 
 
 def usage():
-    print '$Id: stnc2pov.py,v 1.4 2007/05/12 04:37:47 kichiki Exp $'
+    print '$Id: stnc2pov.py,v 1.5 2007/05/13 23:14:10 kichiki Exp $'
     print 'USAGE:'
     print '\t-f or --file : stokes-nc-file'
-    print '\t-nm          : number of mobile particles (default: all particles)'
     sys.exit ()
 
 
@@ -200,40 +339,52 @@ def main():
         if sys.argv[i] == '-f' or sys.argv[i] == '--file':
             filename = sys.argv[i+1]
             i += 2
-        elif sys.argv[i] == '-nm':
-            nm = int(sys.argv[i+1])
-            i += 2
         else:
             usage()
     if filename == '': usage()
 
     nc = stokes.stokes_nc_open (filename)
     #stokes.stokes_nc_print_actives(nc, stokes.get_stdout())
-    if nm == 0:
-        nm = nc.np
+
     lattice = stokes.darray(3)
     stokes.stokes_nc_get_l (nc, lattice)
 
+    # x[] : center of particles
     pos  = stokes.darray(nc.np  * nc.nvec)
-    pos0 = stokes.darray(nc.npf * nc.nvec)
+    # q[] : quaternion
+    if nc.flag_q != 0:
+        q  = stokes.darray(nc.np  * nc.nquat)
+    else:
+        q = []
+    # a[] : radius of mobile particles
     if nc.flag_a != 0:
         a = stokes.darray(nc.np)
         stokes.stokes_nc_get_data0 (nc, "a", a)
     else:
         a = []
+    # af[] : radius of fixed particles
     if nc.flag_af != 0:
         af = stokes.darray(nc.npf)
         stokes.stokes_nc_get_data0 (nc, "af", af)
     else:
         af = []
 
+    # xf0[]
+    if nc.npf > 0:
+        xf0 = stokes.darray(nc.npf * nc.nvec)
+        stokes.stokes_nc_get_data0 (nc, "xf0", xf0)
+
     if lattice[0] != 0.0 or lattice[1] != 0.0 or lattice[2] != 0.0:
         # periodic boundary
-        camera = [0.5 * lattice[0], -110, 0.5 * lattice[2]]
+        if lattice[0] > lattice[2]:
+            l = lattice[0]
+        else:                  
+            l = lattice[2]
+        #camera = [0.5 * lattice[0], -110, 0.5 * lattice[2]]
+        camera = [0.5 * lattice[0], -1.7*l, 0.5 * lattice[2]]
         lookat = [0.5 * lattice[0],  0.0, 0.5 * lattice[2]]
 
-    i = 0
-    while 1:
+    for i in range(nc.ntime):
         file = 'test%04d.pov'%i
         try:
             f = open(file, 'w')
@@ -242,49 +393,64 @@ def main():
             sys.exit()
 
         stokes.stokes_nc_get_data (nc, "x", i, pos)
-        stokes.stokes_nc_get_data0 (nc, "xf0", pos0)
 
         if lattice[0] == 0.0 and lattice[1] == 0.0 and lattice[2] == 0.0:
             # non-periodic boundary
-            (cx,cy,cz, lx,ly,lz) = bounding_box (nm, pos)
+            (cx,cy,cz, lx,ly,lz) = bounding_box (nc.np, pos)
             if lx > lz:
                 l = lx
             else:                  
                 l = lz
+            # prevent to go far away
+            if l > 50: l = 50
+
             camera = [cx, cy-2*l, cz]
             lookat = [cx, cy,   cz]
             write_pov_header_open (f, lattice, camera, lookat)
+
         else:
             # periodic boundary
-            move_camera (camera, lookat)
+            #move_camera (camera, lookat)
             write_pov_header (f, lattice, camera, lookat)
 
-        for j in range(nm):
-            x = pos[j*3]
-            y = pos[j*3+1]
-            z = pos[j*3+2]
-            write_pov_particle (f, x, y, z, 1.0)
-        # in case nc.np contains both mobile and fixed
-        #print '# np = %d, nm = %d'%(nm, nc.np)
-        for j in range(nm, nc.np):
-            x = pos[j*3]
-            y = pos[j*3+1]
-            z = pos[j*3+2]
-            if a != []:
-                write_pov_particle_fixed (f, x, y, z, a[j])
-            else:
-                write_pov_particle_fixed (f, x, y, z, 1.0)
+        if nc.flag_q != 0:
+            # with quaternion
+            stokes.stokes_nc_get_data (nc, "q", i, q)
+            for j in range(nc.np):
+                x = pos[j*3]
+                y = pos[j*3+1]
+                z = pos[j*3+2]
+                if a != []:
+                    write_pov_particle_Q (f, x, y, z, a[j],\
+                                          [q[j*4+0],q[j*4+1],\
+                                           q[j*4+2],q[j*4+3]])
+                else:
+                    write_pov_particle_Q (f, x, y, z, 1.0,\
+                                          [q[j*4+0],q[j*4+1],\
+                                           q[j*4+2],q[j*4+3]])
+        else:
+            # no quaternion
+            for j in range(nc.np):
+                x = pos[j*3]
+                y = pos[j*3+1]
+                z = pos[j*3+2]
+                if a != []:
+                    write_pov_particle (f, x, y, z, a[j])
+                else:
+                    write_pov_particle (f, x, y, z, 1.0)
+
+        # fixed particles
         for j in range(nc.npf):
-            x = pos0[j*3]
-            y = pos0[j*3+1]
-            z = pos0[j*3+2]
+            x = xf0[j*3]
+            y = xf0[j*3+1]
+            z = xf0[j*3+2]
             if af != []:
                 write_pov_particle_fixed (f, x, y, z, af[j])
             else:
                 write_pov_particle_fixed (f, x, y, z, 1.0)
-        f.close()
 
-        i += 1
+        # done
+        f.close()
 
 
 if __name__ == "__main__":
