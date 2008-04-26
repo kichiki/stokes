725f3e5f144e9f64878acd96a04cd46be6469271
kengo ichiki <kengoichiki@gmail.com>
2008-04-26 19:06:25 +0000

    add EV_DH handling. add examples for bonds, angles, and ev-dh. add comments on the unit of length.

--- src/stokes3.scm
+++ src/stokes3.scm
@@ -1,6 +1,6 @@
 ; sample initialization file for stokes3
 ; SC lattice config of 8 particles in (5,5,5) box
-; $Id: stokes3.scm,v 1.10 2008/04/16 00:36:21 kichiki Exp $
+; $Id: stokes3.scm,v 1.11 2008/04/26 19:06:25 kichiki Exp $
 
 ;; output parameters
 (define outfile    "stokes3.SC8.nc") ; output filename
@@ -119,8 +119,9 @@
 
 ;; Brownian dynamics parameters
 (define peclet -1)      ; peclet number (negative means no Brownian force)
-(define length 1.0)     ; unit of the length scale [micro m].
+(define length 1.0)     ; the characteristic length [nm] (or [micro m]).
                         ; the parameters above are recognized by this.
+                        ; the unit should be the same for parameters below
                         ; just ignored for non-Brownian case (peclet<0).
 (define BD-seed 0)      ; seed for random number generator for BD.
 
@@ -150,10 +151,91 @@
 (define flag-relax #f) ; #f => stokesian dynamics with bond interactions
                        ; #t => relaxation dynamics only with bond interactions
 (define gamma 1.0)     ; friction coefficient for relaxation dynamics
+; an example of bonds
+; note that the length unit should be the same for "length" above.
+;(define bonds '(
+;  (; bond 1
+;   0         ; 1) spring type
+;   (         ; 2) spring parameters (list with 3 elements)
+;    0        ;    fene = 0 means (p1, p2) = (A^{sp}, L_{s})
+;    1.0      ;    p1   = A^{sp}, scaled spring constant  (for fene == 0)
+;    2.1)     ;    p2   = L_{s} / a, scaled max extension (for fene == 0)
+;   ((0 1)    ; 3) list of pairs
+;    (1 2)
+;    (2 3))
+;    -1)      ; 4) number of exclusion for lubrication
+;             ;    negative means all particles in the chain is excluded.
+;  (; bond 2
+;   2         ; 1) spring type
+;   (         ; 2) spring parameters (list with 3 elements)
+;    1        ;    fene = 1 means (p1, p2) = (N_{K,s}, b_{K})
+;    19.8     ;    p1 = N_{K,s}, the Kuhn steps for a spring (for fene = 1)
+;    106.0)   ;    p2 = b_{K} [nm](or[micro m]), the Kuhn length (for fene = 1)
+;   ((4 5)    ; 3) list of pairs
+;    (5 6)
+;    (6 7))
+;     1)      ; 4) number of exclusion for lubrication
+; ))
+; where spring types are
+;   0 : Hookean spring (Asp * (r - Ls)
+;   1 : wormlike chain (WLC)
+;   2 : inverse Langevin chain (ILC)
+;   3 : Cohen's Pade approximation
+;   4 : Warner spring
+;   5 : Hookean spring (Asp * r / Ls)
+;   6 : Hookean spring for dWLC
 
 ;; excluded volume parameters
-(define ev-v   '())    ; v [(micro m)^3] for each chain type
-(define ev-lim 5.0)    ; max distance for F^{EV} [micro m]
+; note that the length unit should be the same for "length" above.
+(define ev-v   '())    ; v [nm^3] (or [micro m^3]) for each chain type
+(define ev-lim 5.0)    ; max distance for F^{EV} [nm] (or [micro m])
 
 ;; angle parameters
 (define angles '())
+; an example
+;(define angles '(
+;  (; angle type 1
+;   10.0    ; 1) constant (k^{angle})
+;   0.0     ; 2) angle in degree (theta_0)
+;   0       ; 3) scale flag (0 == scaled)
+;           ;    in this case, the above value for k is just used.
+;   ((0 1 2); 4) list of triplets
+;    (1 2 3)
+;    (2 3 4)
+;   )
+;  )
+;  (; angle type 2
+;   20.0    ; 1) constant (k^{angle})
+;   90.0    ; 2) angle in degree (theta_0)
+;   1       ; 3) scale flag (1 == not scaled yet)
+;           ;    in this case, the potential is given by 
+;           ;    (k/2) * kT * (theta - theta_0)^2
+;   ((3 4 5); 4) list of triplets
+;    (4 5 6)
+;   )
+;  )
+;))
+
+;; excluded volume in Debye-Huckel type
+(define ev-dh   '())
+; an example
+; note that the length unit should be the same for "length" above.
+;(define ev-dh '(
+;  ; system parameters
+;  4.0      ; 1) max distance for EV_DH interaction [nm] (or [micro m])
+;  298.0    ; 2) temperature [K]
+;  80.0     ; 3) dielectric constant of the solution
+;  3.07     ; 4) Debye length [nm] (or [micro m])
+;  (        ; 5) list of chain types
+;   (; chain type 1
+;    2.43    ; 1) nu [e/nm] (or [e/micro m])
+;    5.00    ; 2) l0 [nm] (or [micro m])
+;    (0 1 2) ; 3) list of particles
+;   )
+;   (; chain type 2
+;    2.00    ; 1) nu [e/nm] (or [e/micro m])
+;    4.00    ; 2) l0 [nm] (or [micro m])
+;    (3 4)   ; 3) list of particles
+;   )
+;  )
+;))
